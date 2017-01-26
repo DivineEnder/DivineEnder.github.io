96d0b27136e94cb5ef4d79ddc38fdd04532f73e8
David Anuta <danuta@u.rochester.edu>
2017-01-26 15:33:55 -0500

    Customizing website
    Updated some website icons and main page headers. Currently working on trying to update the navbar so that the HTML code for it does not need to be written in every page.

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+/*
+* @Author: David Anuta
+* @Date:   2017-01-26 15:29:44
+* @Last Modified by:   David Anuta
+* @Last Modified time: 2017-01-26 15:30:16
+*/
+
+$(document).ready(function(){
+  $('.nav').load("partials/nav_bar.html");
+});
