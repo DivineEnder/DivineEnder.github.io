9abb3bda226923b969b0d37d9e9ffab7d6714edd
DivineEnder <qwertydraw@gmail.com>
2017-01-28 16:16:40 -0500

    Updating homepage jumbotron
    Updated the homepage jumbotron with various animations. Added several libs to do this including JQuery js, Morphist, and animate CSS. Libs are located in new libs folder and are loaded in index.html for use. Jumbotron now contains several keywords animated to fade in and out. 'Website Under Construction' text now pulses to catch the eye.

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$("#js-rotating").Morphist({
+	// In animation
+	animateIn: "fadeInLeft",
+	// Out animation
+	animateOut: "fadeOutRight",
+	// Speed of animation
+	speed: 3000,
+	complete: function () {
+		// Stuff to do after animate called
+	}
+});
